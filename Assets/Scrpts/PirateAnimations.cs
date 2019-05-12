9c4be3ed734c2090d6d83fbbad42ade76e757b18
KoreAlonso <andreastur96@gmail.com>
2019-05-12 14:12:31 +0200

    Initial commit

---
+++ Assets/Scrpts/PirateAnimations.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateAnimations : MonoBehaviour {
+
+    public Animator animator;
+
+
+
+
+    void FixedUpdate () {
+        
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+       
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("isAngry");
+
+        }
+        else
+        {
+            animator.SetTrigger("isntAngry");
+
+        }
+        if (Input.GetKey(KeyCode.LeftShift) & vertical > -0.4f) {
+            animator.SetBool("isRun",true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+
+        animator.SetFloat ("Vertical", vertical);
+        animator.SetFloat ("Horizontal", horizontal);
+    }
+}
+
+
+
+
+/*  Estube barajando distintas posibilidades a partir de estas dos, y ninguna obtuvo resultado
+ *  hasta que hioce uso del parametro Trigger. 
+ *  
+ *  
+    if(Input.GetKeyDown(KeyCode.Q){
+
+    animator.SetBool("bool", true);
+    }
+    else
+    { 
+    animator.SetBool("bool", false);
+    }
+
+    -----------------------------------
+    
+    variable = Input.GetKeyDown(KeyCode.Q);
+    animator.SetBool("bool", variable);
+
+
+    */
---
+++ Assets/Scrpts/PirateController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour {
+
+    public float velocity = 3;
+    public float rotate = 55f;
+    public float gravity = 0.2f;
+    public float jumpForce = 5f;
+    public float velocityRun = 4.7f;
+
+
+    public CharacterController characterController;
+    Vector3 direccion = Vector3.zero;
+
+   void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+
+        if (characterController.isGrounded) {
+
+            float movement = Input.GetAxis("Vertical");
+            direccion = new Vector3(0, 0, movement);
+            direccion = transform.TransformDirection(direccion);
+            direccion *= velocity;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                direccion *= velocityRun;
+                
+            }
+
+         if (Input.GetButton ("Jump")) 
+         {
+                direccion.y = jumpForce;      
+         }
+
+        }
+        else
+        {
+            direccion.y -= gravity;
+
+        }
+        transform.Rotate(0, rotate  * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+        characterController.Move(direccion * Time.deltaTime);
+	}
+ 
+}
